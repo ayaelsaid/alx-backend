7c84edc82d6e7265ac68541c1c1fe4515f1b72ef
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-24 22:10:49 +0300

    Update 0-basic_cache.py

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -21,7 +21,10 @@ class BasicCache(BaseCaching):
     def get(self, key):
         """ Get an item by key
         """
-        if key is None or key not in self.cache_data:
+        if key is None:
             return None
-    
+
+        if key not in self.cache_data:
+            return None
+
         return self.cache_data.get(key)
