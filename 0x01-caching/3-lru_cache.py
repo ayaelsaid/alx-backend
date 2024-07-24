00a77dfb43f2fa37c6e010310d746355ea5caca8
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-24 21:48:36 +0300

    Create 3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+'''Task3: lru_cache
+'''
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    class LRUCache that inherits from BaseCaching
+    """
+
+    def __init__(self):
+        """ Initialize the LRU cache
+        """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lru_key = self.order.pop(0)
+                del self.cache_data[lru_key]
+                print(f"DISCARD: {lru_key}")
+
+            self.order.append(key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data.get(key)
