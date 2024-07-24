eb93799dc796f017fb6d493dfa9f4dd4e620637b
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-24 22:09:36 +0300

    Update 100-lfu_cache.py

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -52,7 +52,10 @@ class LFUCache(BaseCaching):
     def get(self, key):
         """ Get an item by key
         """
-        if key is None or key not in self.cache_data:
+        if key is None:
+            return None
+
+        if key not in self.cache_data:
             return None
 
         freq = self.freq[key]
