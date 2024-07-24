a242d7ec1ac6e2fb79b0ebde6a04b9d10566c482
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-07-24 21:26:38 +0300

    Create 1-fifo_cache.py

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""
+task-1
+"""
+
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+  """
+  class FIFOCache that inherits from BaseCaching
+  """
+
+    def __init__(self):
+        """ Initialize the FIFO cache
+        """
+        super().__init__()
+        self.order = []  # Keep track of the order of keys
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is None or item is None:
+            return
+
+        if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            oldest_key = self.order.pop(0)
+            del self.cache_data[oldest_key]
+            print(f"DISCARD: {oldest_key}")
+
+        if key not in self.cache_data:
+            self.order.append(key)
+
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+      
