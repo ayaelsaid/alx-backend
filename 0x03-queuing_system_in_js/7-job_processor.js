b10b94e430d1859c891cbdfaff999a8a2aceb289
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-08-27 23:19:20 +0300

    Create 7-job_processor.js

---
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -0,0 +1,31 @@
+import kue from 'kue';
+
+const blacklisted = [4153518780, 4153518781];
+function sendNotification(phoneNumber, message, job, done){
+  job.progress(0, 100);
+if (blacklisted.includes(phoneNumber)) {
+    const errorMsg = `Phone number ${phoneNumber} is blacklisted`;
+    console.error(`Error: ${errorMsg}`);
+    
+    return done(new Error(errorMsg));
+           }
+  else {
+    job.progress(50);
+    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  }
+  
+    setTimeout(() => {
+      job.progress(100);
+      console.log(`Notification sent to ${phoneNumber}`);
+      done(); 
+    }, 2000);
+  }
+}
+
+queue.process('push_notification_code_2', (job, done) => {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
+
+queue.on('error', (err) => {
+  console.error('Queue error:', err);
+});  
