2120414044a752f5034165e550b56001b35e5205
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-08-27 17:31:48 +0300

    Create 1-redis_op.js

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,23 @@
+import redis from 'redis'
+
+const client = redis.createClient()
+
+client.on('ready', () => {
+	console.log('Redis client connected to the server');
+});
+client.on('error', (err) => {
+	  console.error('Redis client not connected to the server: ', err);
+});
+function setNewSchool(schoolName, value){
+  client.set(schoolName, value, redis.print)
+function displaySchoolValue(schoolName){
+      client.get(schoolName, (err,reply) => {
+        if(err) throw err;
+        else {
+        console.log(reply: ${reply});
+        console.log(${schoolName});
+        }
+        client.quit();
+
+      });
+}
