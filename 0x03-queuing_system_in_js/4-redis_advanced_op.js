ffd6fa0b5ee6e453fcb41f55703d81ab0425ddc2
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-08-27 18:58:42 +0300

    Create 4-redis_advanced_op.js

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,28 @@
+import redis from 'redis'
+
+const client = redis.createClient()
+
+client.on('ready', () => {
+	console.log('Redis client connected to the server');
+});
+client.on('error', (err) => {
+	  console.error('Redis client not connected to the server: ', err);
+});
+client.hset('HolbertonSchools', 
+  'Portland', '50',
+  'Seattle', '80',
+  'New York', '20',
+  'Bogota', '20',
+  'Cali', '40',
+  'Paris', '2',
+  redis.print
+);
+client.hgetall('HolbertonSchools', (err,reply) => {
+        if(err) throw err;
+        else {
+        console.log(reply);
+	}
+        client.quit();
+
+      });
+}
