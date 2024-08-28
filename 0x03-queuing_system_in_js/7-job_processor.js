13653ed6d2fea33f7a41b7fc23a890fb964faacf
ayaelsaid <138704027+ayaelsaid@users.noreply.github.com>
2024-08-28 15:17:28 +0300

    Update 7-job_processor.js

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -21,6 +21,7 @@ function sendNotification(phoneNumber, message, job, done) {
   }
 }
 
-queue.process('push_notification_code_2', (job, done) => {
-  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
 });
